50ee06ef8402da1e95ff4446cf66b063c38d8c7d
Tomasz Borejko <borejko.t@gmail.com>
2017-09-15 01:07:19 +0200

    Utils class created / methods with implementation added

---
+++ bookshelf/src/main/java/borco86/com/github/bookshelf/Utils.java
@@ -0,0 +1,39 @@
+package borco86.com.github.bookshelf;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 
+ * Class that provides utility methods
+ *
+ */
+
+public class Utils {
+
+	public void sortList(List<Book> unsortedList) {
+		Collections.sort(unsortedList);
+	}
+
+	public String formatTitle(String title) {
+		return title.replaceAll("\\s+", "").toLowerCase();
+	}
+
+	private String limitStringLength(String title) {
+		String limitedTitle;
+		if (title.length() <= 32) {
+			limitedTitle = title;
+		} else {
+			limitedTitle = title.substring(0, 31);
+		}
+		return limitedTitle;
+	}
+	
+	public String[] limitStringsLength(String title, String author) {
+		String limitedTitle = limitStringLength(title);
+		String limitedAuthor = limitStringLength(author);
+		String[] array = {limitedTitle,limitedAuthor};
+		return array;
+	}
+
+}
